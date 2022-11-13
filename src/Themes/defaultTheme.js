eb6b935cf40e0c150b979e91b6620a4aabba7a9d
thenorrock <thenowrock@gmail.com>
2022-11-12 23:55:15 -0500

    full landins-mobile-la-fla

---
+++ src/Themes/defaultTheme.js
@@ -0,0 +1,13 @@
+module.exports = {
+  primary: '#C3D82D',
+  secondary: '#4796E1',
+  accent: '#1C1F21',
+  neutral: '#989798',
+  'base-100': '#ECEDEE',
+  'base-200': '#0A3B4A',
+  'base-300': '#03103A',
+  info: '#1eb854',
+  success: '#182c9b',
+  warning: '#E9c40c',
+  error: '#E71D42',
+}
